d5573283b1df99ed3850ec51cd7d7c39525f0a2f
Renata Pulz <renata.pulz@raizen.com>
2024-03-29 01:29:11 -0300

    Adicionado props, depois propTypes e por último inserido lógica para exibição dos itens na tela.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import CardTrilha from "./assets/components/CardTrilha/CardTrilha";
 
 function App() {
+  const listaTrilhas = [{
+    nomeTrilha: "Trilha Lagoinha do Leste",
+    cidade: "Florianópolis",
+    estado: "SC",
+    duracao: 120,
+    trajeto: 1,
+    dificuldade: "média/alta",
+    tipo: "trekking",
+    nomeUsuario: "Renata Pulz",
+    UrlImage: "https://images.pexels.com/photos/917510/pexels-photo-917510.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+  }]
+
   return (
     <div>
-      <p>CardTrilha criado</p>
-      <CardTrilha />
+      {listaTrilhas.map((trilha, index) => (
+        <CardTrilha dadosTrilha={trilha} key={index} />
+      ))}
     </div>
   );
 }
--- src/assets/components/CardTrilha/CardTrilha.jsx
+++ src/assets/components/CardTrilha/CardTrilha.jsx
@@ -1,9 +1,33 @@
-function CardTrilha() {
-    return (
-      <div>
-        <p>Limpo!</p>
-      </div>
-    );
-  }
-  
-  export default CardTrilha;
+import PropTypes from 'prop-types';
+
+function CardTrilha({ dadosTrilha }) {
+  return (
+    <div>
+      <h1>{dadosTrilha.nomeTrilha}</h1>
+      <p>Cidade: {dadosTrilha.cidade}</p>
+      <p>Estado: {dadosTrilha.estado}</p>
+      <p>Duração: {dadosTrilha.duracao} minutos</p>
+      <p>Trajeto: {dadosTrilha.trajeto}</p>
+      <p>Dificuldade: {dadosTrilha.dificuldade}</p>
+      <p>Tipo: {dadosTrilha.tipo}</p>
+      <p>Nome do Usuário: {dadosTrilha.nomeUsuario}</p>
+      <img width={200} src={dadosTrilha.UrlImage} alt="Imagem trilha" />
+    </div>
+  );
+}
+
+CardTrilha.propTypes = {
+  dadosTrilha: PropTypes.shape({
+    nomeTrilha: PropTypes.string.isRequired,
+    cidade: PropTypes.string.isRequired,
+    estado: PropTypes.string.isRequired,
+    duracao: PropTypes.number.isRequired,
+    trajeto: PropTypes.number.isRequired,
+    dificuldade: PropTypes.string.isRequired,
+    tipo: PropTypes.string.isRequired,
+    nomeUsuario: PropTypes.string.isRequired,
+    UrlImage: PropTypes.string.isRequired,
+  })
+};
+
+export default CardTrilha;
