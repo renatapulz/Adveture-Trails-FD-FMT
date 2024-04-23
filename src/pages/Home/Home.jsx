f0b9c1a402ac07c5c8bf732111fea17e22394e1a
Renata Pulz <renata.pulz@raizen.com>
2024-04-23 14:25:10 -0300

    Salvando as imagens no projeto.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
     return (
         <div>
             <div className="container-image">
-                    <img className="image-principal" src="https://s3-alpha-sig.figma.com/img/d499/89ee/a6d23a7a9ffd6d136e430c855a72ebc0?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lCBgR7Pyqff8u8MSpH-UVgBRmgzDWPO89CvVWTUVK36oAJSDic0TFiF7MpDBbeQEgmTwR96kWtNSzFP2yEVfxEr6cKkR~I5g4vQ02vhX7lLrG0RYgiPsa8Nkv~1VCpK~C42quIejs4dZC7uZvGgPOe7y6A-SxuRsampbxZyfspUw3R372dO~GnpLnEfWoi44zMHxu~12qRqD3DiMCduubXZLxlpG1hDEsVuQc6F4TRB-XyV-vnlMj2iVsFx8-MIvocK2xE-jti4n~7r75dMhHLTVPy22FDNxcEiMZtxbJrQNXKIyagTyxYipldkopUnaY6~MGItO-Bdyipg~NBzatw__"></img>
+                    <img className="image-principal" src="\src\assets\images\img-home.jpg"></img>
                     <div className="text-image">
                         <h1>Que tal aproveitar um tempo <br />com a natureza?</h1>
                         <p>Junte-se à comunidade de entusiastas ao ar livre, compartilhe suas aventuras e inspire-se com as experiências de outros aventureiros. Prepare-se para explorar novos horizontes e se conectar com a natureza através do Adventure Trails!</p>
@@ -26,7 +26,7 @@ function Home() {
                         <h1>Compartilhe fotos,<br />dicas e localização das<br />suas trilhas favoritas</h1>
                         <Link to="/cadastro-trilhas"><button className="buttom-home buttom-register">Cadastrar nova trilha</button></Link>
                     </div>
-                    <img className="image-section" src="https://s3-alpha-sig.figma.com/img/749a/d3cd/b59b552226c32b61fb8ba0ba617adccb?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NGwjSnDaaKjKexnlWu1ilnUx6YJb1BX6YieK~FC2nlRpPiOasMRO~AP3FP45EnCtHAZ7p2I~2jTq3P1GvnJ0jouuxFzflK6SBchxsbYzVzfXKiNhhz6tE7ahAT8zig2kt9whVF5d0KEUyt5axCosM15nhnXy43DIBsY6sXhHtXyfFYaai1ucxjdWf2x3aBHsYkXV6PMLsmEbhdLR4Bt8dzoiPgywb63o5rgUFm7HitTMI9I0MlEnT-6piPXEvZm7YZfRen1kTsHf99~tSueQdaIpBOheAiufn4e3KVQoHaq8IZhpv~KGtJZ7tvIAXAcotvxenDlM7lKeK~uovykcYQ__"></img>
+                    <img className="image-section" src="\src\assets\images\img-section.jpg"></img>
                 </section>
             </div>
         </div>
--- src/pages/Listagem-Trilhas/Listagem.jsx
+++ src/pages/Listagem-Trilhas/Listagem.jsx
@@ -12,7 +12,7 @@ function Listagem() {
 
   return (
     <div>
-      <img className="image-tela-lista-trilha" src="https://s3-alpha-sig.figma.com/img/d499/89ee/a6d23a7a9ffd6d136e430c855a72ebc0?Expires=1713744000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lCBgR7Pyqff8u8MSpH-UVgBRmgzDWPO89CvVWTUVK36oAJSDic0TFiF7MpDBbeQEgmTwR96kWtNSzFP2yEVfxEr6cKkR~I5g4vQ02vhX7lLrG0RYgiPsa8Nkv~1VCpK~C42quIejs4dZC7uZvGgPOe7y6A-SxuRsampbxZyfspUw3R372dO~GnpLnEfWoi44zMHxu~12qRqD3DiMCduubXZLxlpG1hDEsVuQc6F4TRB-XyV-vnlMj2iVsFx8-MIvocK2xE-jti4n~7r75dMhHLTVPy22FDNxcEiMZtxbJrQNXKIyagTyxYipldkopUnaY6~MGItO-Bdyipg~NBzatw__"></img>
+      <img className="image-tela-lista-trilha" src="\src\assets\images\img-listagem.jpg"></img>
       <div className="container">
         <div className="container-listagem">
           <h1 className="titulo">Explore Trilhas Incríveis</h1>
